a31dab7f5b1028d7ec8dfc5beee8420c122573cf
Jairah <91213356+jairahflores@users.noreply.github.com>
2024-03-24 13:11:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From chroot300" }');
+    res.send('{ "response": "Hello, this is a test" }');
 });
 
 app.get('/hello', function (req, res) {
