5da0c64aaf9aa84265dbef39ec0867ff4d64776e
jairah.flores <jairah.flores@ibm.com>
2024-03-24 16:22:04 +0800

    edit apicall /hello

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var request = require('supertest');
 var app = require('../index.js');
-describe('GET /will', function() {
+describe('GET /hello', function() {
     it('respond with hello world', function(done) {
-        request(app).get('/will').expect('{ "response": "Hello World" }', done);
+        request(app).get('/hello').expect('{ "response": "Hello World" }', done);
     });
 });
